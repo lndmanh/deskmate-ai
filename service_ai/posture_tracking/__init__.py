e155fda627059c7ad91b7cf314bdc083f0a114e6
lleon <phongepic1107@gmail.com>
2026-06-27 13:55:21 +0700

    JSON db

--- service_ai/posture_tracking/__init__.py
+++ service_ai/posture_tracking/__init__.py
@@ -2,6 +2,7 @@ from .analyzer import PostureAnalyzer
 from .calibration import CalibrationResult, create_posture_calibration
 from .constants import BLAZEPOSE_LANDMARKS, DEFAULT_POSTURE_THRESHOLDS
 from .geometry import extract_posture_features
+from .session_recorder import PostureSessionRecorder
 from .types import (
     PoseFrame,
     PoseLandmark,
@@ -27,6 +28,7 @@ __all__ = [
     "PostureEvent",
     "PostureFeatures",
     "PostureIssue",
+    "PostureSessionRecorder",
     "PostureThresholds",
     "create_posture_calibration",
     "extract_posture_features",
